b3625cc2e593ed12678dad0fe0a7165c3ea37785
aspexp <ipathipad@gmail.com>
2024-05-19 13:55:03 +0700

    update ID from document._id

--- product/product.go
+++ product/product.go
@@ -9,20 +9,20 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Product struct {
+	ID primitive.ObjectID `bson:"_id"`
 	Product_Id int64 `bson:"product_id,omitempty"`
 	Product_Name string `bson:"product_name,omitempty"`
 	Retail_Price float64 `bson:"retail_price,omitempty"`			
 }
 func GetProduct(client *mongo.Client, c echo.Context) error{
 
-			// uri := os.Getenv("MONGODB_URI")
-			// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-
-			var result []bson.D
+			// var result []bson.D
+			var result []Product 
 			consumerDatabase := client.Database("consumer")
 			productCollection := consumerDatabase.Collection("products")
 			cursor, err := productCollection.Find(context.TODO(), bson.D{})
@@ -30,11 +30,11 @@ func GetProduct(client *mongo.Client, c echo.Context) error{
 				panic(err)
 			}
 			for cursor.Next(context.TODO()){
-				var r bson.D
-				if err := cursor.Decode(&r); err != nil {
+				var p Product
+				if err := cursor.Decode(&p); err != nil {
 					log.Fatal(err)
 				}
-				result = append(result, r)
+				result = append(result, p)
 
 			}
 			return c.JSON(http.StatusOK, result )
